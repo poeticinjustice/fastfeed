8eaa7a064dbb599a797d5d4462bc16d8c3afb6ba
Ramzi Kanazi <arcan3xx>
2016-08-19 13:38:06 -0400

    working with routes

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,10 @@
 $(document).ready(function(){
   console.log('/public/js/script.js is loaded')
 
-  $('#newsButton').on('click', function(){
+  $('#techButton').on('click', function(){
     $('#newsapi_column ul').empty();
     $.ajax({
-      url: '/key/newsapi',
+      url: '/key/tech',
       dataType: 'json',
       success: function (data) {
         var results = data.articles;
@@ -23,5 +23,44 @@ $(document).ready(function(){
     });
   });
 
+  $('#hackernewsButton').on('click', function(){
+    $('#newsapi_column ul').empty();
+    $.ajax({
+      url: '/key/hackernews',
+      dataType: 'json',
+      success: function (data) {
+        var results = data.articles;
+        $(results).each(function(index) {
+          var content = results[index];
+            $('#newsapi_column ul').append(
+            $('<li />', {html: '<a href="' + content.url + '" target="_blank"><strong>'+ content.title +'</a></strong><br>' + content.description + ' <button>Add</button>'})
+          );
+        });
+      },
+      error: function () {
+        $("#newsapi_column").html('<p>Error: ' + error + '</p>');
+      }
+    });
+  });
+
+  $('#recodeButton').on('click', function(){
+    $('#newsapi_column ul').empty();
+    $.ajax({
+      url: '/key/recode',
+      dataType: 'json',
+      success: function (data) {
+        var results = data.articles;
+        $(results).each(function(index) {
+          var content = results[index];
+            $('#newsapi_column ul').append(
+            $('<li />', {html: '<a href="' + content.url + '" target="_blank"><strong>'+ content.title +'</a></strong><br>' + content.description + ' <button>Add</button>'})
+          );
+        });
+      },
+      error: function () {
+        $("#newsapi_column").html('<p>Error: ' + error + '</p>');
+      }
+    });
+  });
 
 });
--- routes/key.js
+++ routes/key.js
@@ -5,11 +5,34 @@ const request   = require('request');
 
 const naKey     = process.env.NEWSAPI_KEY;
 
-router.get('/newsapi', function(req, res) {
+const tech      = ['/hackernews', '/recode']
+
+router.get('/tech', function(req, res) {
+  request(tech,
+    function (error, response, body) {
+      res.send(body);
+    }
+  )
+})
+
+router.get('/hackernews', function(req, res) {
+  request('https://newsapi.org/v1/articles?source=hackernews&sortBy=top&apiKey='+naKey,
+    function (error, response, body) {
+      res.send(body);
+    }
+  )
+})
+
+router.get('/recode', function(req, res) {
   request('https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey='+naKey,
     function (error, response, body) {
       res.send(body);
-  })
+    }
+  )
 })
 
 module.exports = router;
+
+
+// pass array
+// http://stackoverflow.com/questions/15350025/express-js-single-routing-handler-for-multiple-routes-in-a-single-line
