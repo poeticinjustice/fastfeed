76d412733a7d6ba0715c4c92accb5b193b0f1245
Ramzi Kanazi <arcan3xx>
2016-08-22 20:04:33 -0400

    heroku update 1,000,001

--- db/db.js
+++ db/db.js
@@ -21,7 +21,7 @@ const pg              = require('pg-promise')({});
 
 const pgConfig        = { host: process.env.PG_HOST,
                           port: process.env.PG_PORT,
-                          database: 'fastfeed',
+                          database: process.env.PG_DATABASE || 'fastfeed',
                           user: process.env.PG_USER,
                           password: process.env.PG_PASSWORD };
 
